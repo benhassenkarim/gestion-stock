68d08b0b9e273e60de0ac6fb9c9bbe869bf8daa9
benhassenkarim <benhassenkarim199@gmail.com>
2023-05-11 13:31:06 +0200

    show  article list

--- src/app/composants/detail-article/detail-article.component.ts
+++ src/app/composants/detail-article/detail-article.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {ArticleDto} from "../../../gs-api/src/models/article-dto";
+import {Router} from "@angular/router";
+import {ArticleService} from "../../service/article/article.service";
 
 @Component({
   selector: 'app-detail-article',
   templateUrl: './detail-article.component.html',
   styleUrls: ['./detail-article.component.css']
 })
-export class DetailArticleComponent {
+export class DetailArticleComponent implements OnInit{
 
+  @Input()
+  articleDto: ArticleDto = {};
+  @Output()
+  suppressionResult = new EventEmitter();
+
+  constructor(
+    private router: Router,
+    private articleService: ArticleService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  modifierArticle(): void {
+    this.router.navigate(['nouvelarticles', this.articleDto.id]);
+  }
+
+  confirmerEtSupprimerArticle(): void {
+    if (this.articleDto.id) {
+      this.articleService.deleteArticle(this.articleDto.id)
+        .subscribe(res => {
+          this.suppressionResult.emit('success');
+        }, error => {
+          this.suppressionResult.emit(error.error.error);
+        });
+    }
+  }
 }
